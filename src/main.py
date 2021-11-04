67ec63dc84fdc15b977740746af17450c3da8218
Richard Williams <richardlwilliams@protonmail.com>
2021-11-04 11:02:28 -0600

    Add handler class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import gi
-from zoeppritz import Zoeppritz
+from model import Model
 
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
@@ -8,31 +8,33 @@ from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.figure import Figure
 
+class Handler():
+    def on_adjustment_p1_changed(self, adjustment):
+        print(adjustment.get_value())
+
+    def on_main_window_destroy(self, widget, data=None):
+        Gtk.main_quit()
+
 class MainWindow(Gtk.Window):
-    
-    a1, a2 = 2000.,4000.
-    b1, b2 = 1070.,2310.
-    p1, p2 = 2000.,2500.
-    zoep = Zoeppritz()
-    fig = zoep.plot_energy_coeff(a1,a2,b1,b2,p1,p2)
+    model = Model()
 
     def __init__(self):
         #get gui from glade file
         self.builder = Gtk.Builder()
         self.builder.add_from_file("gui.glade")
-        self.builder.connect_signals(self)
+        self.builder.connect_signals(Handler())
 
         #connect graph
         self.graph = self.builder.get_object("graph_box")
-        canvas = FigureCanvas(self.fig)
+        fig = self.model.generate_energy_graph()
+        canvas = FigureCanvas(fig)
         self.graph.add(canvas)
 
         #display main window
         self.main_window = self.builder.get_object("main_window")
         self.main_window.show_all()
 
-    def on_main_window_destroy(self, widget, data=None):
-        Gtk.main_quit()
+
 
     def main(self):
         Gtk.main()
--- src/model.py
+++ src/model.py
@@ -1,2 +1,13 @@
+from zoeppritz import Zoeppritz
+
 class Model:
-    def __init__(self):
+    vp1 = 2000
+    vp2 = 4000
+    vs1 = 1070
+    vs2 =  2310
+    density1 = 2000
+    density2 = 2500
+
+    def generate_energy_graph(self):
+        zoep = Zoeppritz()
+        return zoep.plot_energy_coeff(self.vp1, self.vp2, self.vs1, self.vs2, self.density1, self.density2)
