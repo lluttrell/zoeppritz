3d9d8bb6e5f7433d0b04fd9b50daa2a1e6f30cb7
Richard Williams <richardlwilliams@protonmail.com>
2021-11-04 08:00:01 -0600

    Finish basic gui

--- src/app.py
+++ src/app.py
@@ -9,10 +9,11 @@ from matplotlib.backends.backend_gtk3agg import (
 from matplotlib.figure import Figure
 
 class MainWindow(Gtk.Window):
-    zoep = Zoeppritz()
+    
     a1, a2 = 2000.,4000.
     b1, b2 = 1070.,2310.
     p1, p2 = 2000.,2500.
+    zoep = Zoeppritz()
     fig = zoep.plot_energy_coeff(a1,a2,b1,b2,p1,p2)
 
     def __init__(self):
@@ -24,15 +25,11 @@ class MainWindow(Gtk.Window):
         #connect graph
         self.graph = self.builder.get_object("graph_box")
         canvas = FigureCanvas(self.fig)
-        # canvas.set_size_request(800, 600)
         self.graph.add(canvas)
 
         #display main window
         self.main_window = self.builder.get_object("main_window")
         self.main_window.show_all()
-
-
-
 
     def on_main_window_destroy(self, widget, data=None):
         Gtk.main_quit()
