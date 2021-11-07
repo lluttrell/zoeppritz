01a0a11a4b7dd03dbe32b0a1a86bbd13d64a6b3f
Richard Williams <richardlwilliams@protonmail.com>
2021-11-07 09:21:52 -0700

    Extract trig functions to property

--- src/model.py
+++ src/model.py
@@ -1,53 +1,73 @@
 import numpy as np
+import cmath as cmath
 
 class Model:
     def __init__(self):
         self.rock1 = Rock(2000,1070,2000)
         self.rock2 = Rock(4000, 2310, 2500)
+        self.theta1 = 45
 
-    def reflected_s_angle(self, theta):
-        return np.arcsin(self.rock1.s_velocity / self.rock1.p_velocity * np.sin(theta))
+    @property
+    def theta1(self):
+        return np.radians(self._theta1_degrees)
 
-    def transmitted_p_angle(self, theta):
-        return np.arcsin(self.rock2.p_velocity / self.rock1.p_velocity * np.sin(theta))
+    @theta1.setter
+    def theta1(self, value):
+        if value < 0 or value > 90:
+            raise ValueError("Incidence angle not in valid range (0,90)")
+        self._theta1_degrees = value
 
-    def transmitted_s_angle(self, theta):
-        return np.arcsin(self.rock2.s_velocity / self.rock1.p_velocity * np.sin(theta))
+    @property
+    def phi1(self):
+        return np.arcsin(self.rock1.s_velocity / self.rock1.p_velocity * np.sin(self.theta1))
 
-    def generate_energy_graph(self, theta):
-        zoep = Zoeppritz()
-        return zoep.plot_energy_coeff(self.vp1, self.vp2, self.vs1, self.vs2, self.density1, self.density2)
+    @property
+    def sin_theta2(self):
+        return  self.rock2.p_velocity / self.rock1.p_velocity * np.sin(self.theta1)
+
+    @property
+    def cos_theta2(self):
+        return cmath.sqrt(1. - self.sin_theta2**2)
+
+    @property
+    def sin_phi2(self):
+        return self.rock2.s_velocity / self.rock1.p_velocity * np.sin(self.theta1)
+
+    @property
+    def cos_phi2(self):
+        return cmath.sqrt(1. - self.sin_phi2**2)
 
     def amplitude_matrix(self, theta_deg):
-        theta1 = np.radians(theta_deg)
-
         vp1 = self.rock1.p_velocity
+        vs1 = self.rock1.s_velocity
+        zp1 = self.rock1.p_impedance
+        zs1 = self.rock1.s_impedance
+        
         vp2 = self.rock2.p_velocity
-        vs1 = self.rock2.s_velocity
         vs2 = self.rock2.s_velocity
-        rho1 = self.rock1.density
-        rho2 = self.rock2.density
+        zs2 = self.rock2.s_impedance
+        zp2 = self.rock2.p_impedance
 
-        theta2 = self.transmitted_p_angle(theta1)
-        phi1 = self.reflected_s_angle(theta1)
-        phi2 = self.transmitted_s_angle(theta1)
+        #double angles
+        s2t2 = 2 * self.sin_theta2 * self.cos_theta2
+        s2d2 = 2 * self.sin_phi2 * self.cos_phi2
+        c2d2 = 1. - 2 * self.sin_phi2**2
 
-        A = np.array([
-            [-np.sin(theta1), -np.cos(phi1), np.sin(theta2), np.cos(phi2)],
-            [np.cos(theta1), -np.sin(phi1), np.cos(theta2), -np.sin(theta2)],
-            [np.sin(2*theta1), vp1 / vp2 * np.cos(2 * phi1), (rho2 * vs2**2 * vp1) / (rho1 * vs1**2 * vp2) * np.sin(2*theta2), (rho2 * vs2 * vp1) / (rho1 * vs1**2) * np.cos(2*phi2)],
-            [-np.cos(2 * phi1), vs1 / vp1 * np.sin(2*phi1), (rho2 * vp2) / (rho1 * vp1) * np.cos(2*phi2), (-rho2 * vs2) / (rho1 * vp1) * np.sin(2*phi2)]
-        ])
+        #Zoepritz equations
+        A = np.array([[np.cos(self.theta1), -np.sin(self.phi1), self.cos_theta2, self.sin_phi2],
+                    [np.sin(self.theta1), np.cos(self.phi1), - self.sin_theta2, self.cos_phi2],
+                    [zp1 * np.cos(2 * self.phi1), -zs1*np.sin(2 *self.phi1), -zp2*c2d2, -zs2*s2d2],
+                    [vs1 / vp1 * np.sin(2 * self.theta1), zs1 * np.cos(2*self.phi1), vs2 / vp2 * zs2 * s2t2, -zs2 * c2d2]])
 
-        B = np.array([
-            [np.sin(theta1)],
-            [np.cos(theta1)],
-            [np.sin(2*theta1)],
-            [np.cos(2*phi1)]
-        ])
+        B = np.array([[np.cos(self.theta1)],
+                    [-np.sin(self.theta1)],
+                    [-zp1 * np.cos(2 * self.phi1)],
+                    [vp1 / vs1 * zs1 * np.sin(2*self.theta1)]])
 
         #Solve the matrix equations
         return np.linalg.solve(A,B)
+
+    
 
 class Rock:
     def __init__(self, p_velocity, s_velocity, density):
